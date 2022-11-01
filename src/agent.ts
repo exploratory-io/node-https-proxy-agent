ace85c8e90247dc45786c9fcdaaf24d642ab7784
Hideaki Hayashi <hihayash@gmail.com>
2022-11-01 06:26:32 -0700

    Pass CA to tls.connect

--- src/agent.ts
+++ src/agent.ts
@@ -142,7 +142,8 @@ export default class HttpsProxyAgent extends Agent {
 				return tls.connect({
 					...omit(opts, 'host', 'hostname', 'path', 'port'),
 					socket,
-					servername
+					servername,
+					ca: proxy.ca
 				});
 			}
 
